daef0eb659ff68ba35b62080170fbc93098272dc
avinashmohare <mohareavinash80@gmail.com>
2023-06-24 23:19:38 +0530

    First Page Animated

--- script.js
+++ script.js
@@ -18,38 +18,93 @@ function revealToSpan() {
     });
 }
 
-revealToSpan();
+function valueSetters() {
+    //fot HomePage
+    gsap.set("#nav a", { y: "-100%", opacity: 0 });
+    gsap.set("#home span .child", { y: "100%" });
+    gsap.set("#home .row img", { opacity: 0 });
 
-var tl = gsap.timeline();
+    //for SVG
+    document.querySelectorAll("#Visual>g").forEach(function (e) {
+        var character = e.childNodes[1].childNodes[1];
 
-tl.from(".child span", {
-    x: 200,
-    stagger: 0.2,
-    duration: 1.4,
-    ease: Power3.easeInOut,
-})
-    .to(".parent .child", {
-        y: "-100%",
-        duration: 1,
-        delay: 0,
-        ease: Circ.easeInOut,
-    })
-    .to("#loader", {
-        height: 0,
-        duration: 1,
-        delay: 0,
-        ease: Circ.easeInOut,
-    })
-    .to("#green", {
-        height: "100%",
-        top: 0,
-        duration: 1,
-        delay: -0.8,
-        ease: Circ.easeInOut,
-    })
-    .to("#green", {
-        height: "0%",
-        duration: 1,
-        delay: -0.3,
-        ease: Circ.easeInOut,
+        character.style.strokeDasharray = character.getTotalLength() + "px";
+        character.style.strokeDashoffset = character.getTotalLength() + "px";
     });
+}
+
+function loaderAnimation() {
+    var tl = gsap.timeline();
+
+    tl.from("#loader .child span", {
+        x: 200,
+        stagger: 0.2,
+        duration: 1.4,
+        ease: Power3.easeInOut,
+    })
+        .to("#loader .parent .child", {
+            y: "-100%",
+            duration: 1,
+            delay: 0,
+            ease: Circ.easeInOut,
+        })
+        .to("#loader", {
+            height: 0,
+            duration: 1,
+            delay: 0,
+            ease: Circ.easeInOut,
+        })
+        .to("#green", {
+            height: "100%",
+            top: 0,
+            duration: 1,
+            delay: -0.8,
+            ease: Circ.easeInOut,
+        })
+        .to("#green", {
+            height: "0%",
+            duration: 1,
+            delay: -0.3,
+            ease: Circ.easeInOut,
+            // onComplete: function () {
+            //     animateHomepage();
+            // },
+
+            onComplete: function () {
+                animateHomepage();
+            },
+        });
+}
+
+function animateHomepage() {
+    var tl = gsap.timeline();
+
+    tl.to("#nav a", {
+        y: 0,
+        opacity: 1,
+        stagger: 0.05,
+        ease: Expo.easeInOut,
+    }).to("#home .parent .child", {
+        y: 0,
+        stagger: 0.1,
+        duration: 1.5,
+        delay: -1,
+        ease: Expo.easeInOut,
+        onComplete: function () {
+            animateSvg();
+        },
+    });
+}
+
+function animateSvg() {
+    gsap.to("#Visual>g>g>path, #Visual>g>g>polyline", {
+        strokeDashoffset: 0,
+        duration: 3,
+        ease: Expo.easeInOut,
+        delay: -1,
+    });
+}
+
+revealToSpan();
+valueSetters();
+loaderAnimation();
